7d831c271160c262911eb06d81d923529a9b05f9
Julian Panucci <julian.panucci@underarmour.com>
2021-04-19 10:54:57 -0500

    Update Package version

--- Package.swift
+++ Package.swift
@@ -1,15 +1,18 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
     name: "SQLite.swift",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [.library(name: "SQLite", targets: ["SQLite"])],
     targets: [
         .target(name: "SQLite", dependencies: ["SQLiteObjc"]),
         .target(name: "SQLiteObjc"),
         .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests")
     ],
-    swiftLanguageVersions: [4, 5]
+    swiftLanguageVersions: [.v5]
 )
 
 #if os(Linux)
